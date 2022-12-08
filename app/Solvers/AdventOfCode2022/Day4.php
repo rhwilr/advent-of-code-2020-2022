718032e9c2b571e28e38ae2c084371e855887ad2
Ralph Huwiler <ralph@huwiler.dev>
2022-12-08 22:51:07 +0100

    Solved 2022 day 4

--- app/Solvers/AdventOfCode2022/Day4.php
+++ app/Solvers/AdventOfCode2022/Day4.php
@@ -8,76 +8,58 @@ use App\Solvers\AbstractSolver;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Str;
 
-class Day3 extends AbstractSolver
+class Day4 extends AbstractSolver
 {
     protected function parse(string $input): Collection
     {
         return Str::of(trim($input))
-            ->explode("\n");
+            ->explode("\n")
+            ->map(function ($line) {
+                return Str::of($line)
+                    ->split("/[-,]+/")
+                    ->map(fn ($item) => intval($item))
+                    ->chunk(2);
+            });
     }
 
     protected function partOne(string $input)
     {
         return $this->parse($input)
-            ->map(function ($rucksack) {
-                $length = mb_strlen($rucksack) / 2;
+            ->filter(function (Collection $line) {
+                [$e1, $e2] = $line->values();
 
-                $first = str_split(substr($rucksack, 0, $length));
-                $second = str_split(substr($rucksack, $length, $length));
-
-                return [
-                    $first,
-                    $second
-                ];
+                return $this->sectionContains($e1, $e2) || $this->sectionContains($e2, $e1);
             })
-            ->map(function ($param) {
-                [$first, $second] = $param;
-
-                $char =  collect($first)
-                    ->intersect(collect($second))
-                    ->unique()
-                    ->first();
-
-                $ascii = ord($char);
-
-                // Uppercase from 65-90 to 27-52
-                if ($ascii < 91) {
-                    return $ascii - 38;
-                }
-
-                // Lowercase from 97-122 to 1-26
-                return $ascii - 96;
-            })
-            ->sum();
+            ->count();
     }
 
     protected function partTwo(string $input)
     {
         return $this->parse($input)
-            ->map(function ($rucksack) {
-                return collect(str_split($rucksack))
-                    ->map(function ($char) {
-                        $ascii = ord($char);
+            ->filter(function (Collection $line) {
+                [$e1, $e2] = $line->values();
 
-                        // Uppercase from 65-90 to 27-52
-                        if ($ascii < 91) {
-                            return $ascii - 38;
-                        }
-
-                        // Lowercase from 97-122 to 1-26
-                        return $ascii - 96;
-                    });
+                return $this->sectionOverlap($e1, $e2) && $this->sectionOverlap($e2, $e1);
             })
-            ->chunk(3)
-            ->map(function (Collection $group) {
-                [$e1, $e2, $e3] = $group->values();
+            ->count();
+    }
 
-                return collect($e1)
-                    ->intersect($e2)
-                    ->intersect($e3)
-                    ->unique()
-                    ->first();
-            })
-            ->sum();
+    // Returns true if the first section is contained within the second section
+    private function sectionContains(Collection $e1, Collection $e2)
+    {
+        [$startE1, $endE1] = $e1->values();
+        [$startE2, $endE2] = $e2->values();
+
+        return $startE1 >= $startE2 && $endE1 <= $endE2;
+    }
+
+
+    // Returns true if the sections have any overlap
+    private function sectionOverlap(Collection $e1, Collection $e2)
+    {
+        [$startE1, $endE1] = $e1->values();
+        [$startE2, $endE2] = $e2->values();
+
+        return !($startE1 <= $endE1 && $endE1 < $startE2 && $startE2 <= $endE2);
     }
 }
--- tests/Unit/AdventOfCode2022/Day4Test.php
+++ tests/Unit/AdventOfCode2022/Day4Test.php
@@ -4,50 +4,50 @@ declare(strict_types=1);
 
 namespace Tests\Unit\AdventOfCode2022;
 
-use App\Solvers\AdventOfCode2022\Day3;
+use App\Solvers\AdventOfCode2022\Day4;
 use Tests\TestCase;
 
-class Day3Test extends TestCase
+class Day4Test extends TestCase
 {
     public function testSolvePartOneExample(): void
     {
-        $day = Day3::make()->setInput(<<<TXT
-vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw
+        $day = Day4::make()->setInput(<<<TXT
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
 TXT);
 
-        $this->assertSame(157, $day->solvePartOne());
+        $this->assertSame(2, $day->solvePartOne());
     }
 
     public function testSolvePartOne(): void
     {
-        $day = Day3::make();
+        $day = Day4::make();
 
-        $this->assertSame(7568, $day->solvePartOne());
+        $this->assertSame(515, $day->solvePartOne());
     }
 
     public function testSolvePartTwoExample(): void
     {
-        $day = Day3::make()->setInput(<<<TXT
-vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw
+        $day = Day4::make()->setInput(<<<TXT
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
 TXT);
 
-        $this->assertSame(70, $day->solvePartTwo());
+        $this->assertSame(4, $day->solvePartTwo());
     }
 
     public function testSolvePartTwo(): void
     {
-        $day = Day3::make();
+        $day = Day4::make();
 
-        $this->assertSame(2780, $day->solvePartTwo());
+        $this->assertSame(883, $day->solvePartTwo());
     }
 }
